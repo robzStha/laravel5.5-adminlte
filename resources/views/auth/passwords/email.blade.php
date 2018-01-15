d15e31d78fa74c8c9ce065b58887bdf21430b1bb
robzstha <fearless.rabin@gmail.com>
2018-01-15 09:24:29 +0545

    First Commit

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,103 @@
+@extends('auth.template')
+
+@section('content')
+    <body class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <b>robz</b>Stha
+        </div>
+
+        <div class="register-box-body">
+            <p class="login-box-msg">Reset Password</p>
+            <form action="{{ route('password.email') }}" method="post">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }} has-feedback">
+                    <input id="email" type="email" class="form-control" name="email"
+                           value="{{ old('email') }}" required autofocus placeholder="Email">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                    @if ($errors->has('email'))
+                    <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                    @endif
+
+                </div>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Reset</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+
+            </form>
+
+
+            <div class="social-auth-links text-center">
+
+            </div>
+            <!-- /.social-auth-links -->
+
+
+            <a href="{{ route('login') }}">
+                Login
+            </a><br>
+
+            <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+
+
+        </div>
+        <!-- /.form-box -->
+
+    </div>
+    <!-- /.register-box -->
+
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-md-8 col-md-offset-2">--}}
+                {{--<div class="panel panel-default">--}}
+                    {{--<div class="panel-heading">Reset Password</div>--}}
+
+                    {{--<div class="panel-body">--}}
+                        {{--@if (session('status'))--}}
+                            {{--<div class="alert alert-success">--}}
+                                {{--{{ session('status') }}--}}
+                            {{--</div>--}}
+                        {{--@endif--}}
+
+                        {{--<form class="form-horizontal" method="POST" action="{{ route('password.email') }}">--}}
+                            {{--{{ csrf_field() }}--}}
+
+                            {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
+                                {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="email" type="email" class="form-control" name="email"--}}
+                                           {{--value="{{ old('email') }}" required>--}}
+
+                                    {{--@if ($errors->has('email'))--}}
+                                        {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                    {{--</span>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group">--}}
+                                {{--<div class="col-md-6 col-md-offset-4">--}}
+                                    {{--<button type="submit" class="btn btn-primary">--}}
+                                        {{--Send Password Reset Link--}}
+                                    {{--</button>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</form>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+
+    </body>
+@endsection
